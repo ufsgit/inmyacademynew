62e9997b598646decf9874c79a1f027688ae29d1
almausuf <almayousuf8@gmail.com>
2026-08-11 14:44:42 +0530

    db

--- db/ima_db.sql
+++ db/ima_db.sql
@@ -16,6 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `admin_documents`
+--
+
+DROP TABLE IF EXISTS `admin_documents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_documents` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(100) NOT NULL,
+  `course_title` varchar(255) DEFAULT NULL,
+  `module_name` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(500) NOT NULL,
+  `uploaded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_documents`
+--
+
+LOCK TABLES `admin_documents` WRITE;
+/*!40000 ALTER TABLE `admin_documents` DISABLE KEYS */;
+INSERT INTO `admin_documents` VALUES (2,'Mastery Battles','Diplomatic Policy Leadership','Module 1','sample text file','dashboard ima ref.png','http://localhost:5001/uploads/1786094564870-dashboard-ima-ref.png','2026-08-07 09:22:44'),(3,'Mastery Battles','Diplomatic Policy Leadership','Module 2','Sample doc','skillstorm competitions global mastery battles (2).pdf','http://localhost:5001/uploads/1786101074223-skillstorm-competitions-global-mastery-battles-(2).pdf','2026-08-07 11:11:14'),(4,'Open Challenges','Entrepreneurship and Innovation Accelerator',NULL,'Guidance','receipt accountant.png','http://localhost:5001/uploads/1786423901683-receipt-accountant.png','2026-08-11 04:51:41'),(5,'School Competitions','Single Team',NULL,'sample doc','study_abroad.png','http://localhost:5001/uploads/1786426549477-study_abroad.png','2026-08-11 05:35:49');
+/*!40000 ALTER TABLE `admin_documents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `challenges`
 --
 
@@ -30,7 +60,7 @@ CREATE TABLE `challenges` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +69,7 @@ CREATE TABLE `challenges` (
 
 LOCK TABLES `challenges` WRITE;
 /*!40000 ALTER TABLE `challenges` DISABLE KEYS */;
-INSERT INTO `challenges` VALUES (1,'Advanced Research Publication',NULL,'Global Mastery Battles','2026-08-06 12:28:03','2026-08-06 12:28:03'),(2,'Legal Advocacy Professional',NULL,'Global Mastery Battles','2026-08-07 04:10:58','2026-08-07 04:10:58'),(3,'Diplomatic Policy Leadership',NULL,'Global Mastery Battles','2026-08-07 04:38:47','2026-08-07 04:38:47'),(4,'Digital Systems Architecture',NULL,'Global Mastery Battles','2026-08-07 04:45:46','2026-08-07 04:45:46'),(5,'Entrepreneurial Visionary Accelerator',NULL,'Global Mastery Battles','2026-08-07 05:01:24','2026-08-07 05:01:24');
+INSERT INTO `challenges` VALUES (1,'Advanced Research Publication',NULL,'Global Mastery Battles','2026-08-06 12:28:03','2026-08-06 12:28:03'),(2,'Legal Advocacy Professional',NULL,'Global Mastery Battles','2026-08-07 04:10:58','2026-08-07 04:10:58'),(3,'Diplomatic Policy Leadership',NULL,'Global Mastery Battles','2026-08-07 04:38:47','2026-08-07 04:38:47'),(4,'Digital Systems Architecture',NULL,'Global Mastery Battles','2026-08-07 04:45:46','2026-08-07 04:45:46'),(5,'Entrepreneurial Visionary Accelerator',NULL,'Global Mastery Battles','2026-08-07 05:01:24','2026-08-07 05:01:24'),(6,'Global Oratory and Communication Certificate',NULL,'Global Open Challenges','2026-08-07 10:12:51','2026-08-07 10:12:51'),(7,'School Competition - Single Team',NULL,'Global School Competitions','2026-08-07 10:21:26','2026-08-07 10:21:26'),(8,'Entrepreneurship and Innovation Accelerator',NULL,'Global Open Challenges','2026-08-07 10:47:42','2026-08-07 10:47:42'),(9,'Digital Media Strategy',NULL,'Global Mastery Battles','2026-08-07 11:38:50','2026-08-07 11:38:50');
 /*!40000 ALTER TABLE `challenges` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,7 +178,7 @@ CREATE TABLE `registrations` (
   `parent_email` varchar(255) DEFAULT NULL,
   `parent_consent` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,7 +187,7 @@ CREATE TABLE `registrations` (
 
 LOCK TABLES `registrations` WRITE;
 /*!40000 ALTER TABLE `registrations` DISABLE KEYS */;
-INSERT INTO `registrations` VALUES (1,'ss',22,'Algeria','kollam','ma@gmail.com','123456',1.00,'Advanced Research Publication, Diplomatic Policy Leadership','2026-08-06 12:32:26','{}',NULL,NULL,NULL,NULL,NULL,0),(2,'swaroop',22,'Algeria','samb','sw@gmail.com','123456',1.00,'Legal Advocacy Professional','2026-08-07 04:10:58',NULL,NULL,NULL,NULL,NULL,NULL,0),(3,'Alma Yousuf',23,'India','Alappuzha','al@gmail.com','123456',1.00,'Entrepreneurial Visionary Accelerator, Digital Systems Architecture','2026-08-07 05:05:09','{\"trailblazersCount\":0,\"visioneersCount\":0,\"strategistsCount\":0,\"studentNames\":{}}',NULL,NULL,NULL,NULL,NULL,0),(4,'Riju ',23,'Cabo Verde','sab','ri@gmail.com','123456',1.00,'Diplomatic Policy Leadership, Legal Advocacy Professional','2026-08-07 06:24:10','{}',NULL,NULL,NULL,NULL,NULL,0);
+INSERT INTO `registrations` VALUES (1,'ss',22,'Algeria','kollam','ma@gmail.com','123456',1.00,'Advanced Research Publication, Diplomatic Policy Leadership','2026-08-06 12:32:26','{}',NULL,NULL,NULL,NULL,NULL,0),(2,'swaroop',22,'Algeria','samb','sw@gmail.com','123456',1.00,'Legal Advocacy Professional','2026-08-07 04:10:58',NULL,NULL,NULL,NULL,NULL,NULL,0),(3,'Alma Yousuf',23,'India','Alappuzha','al@gmail.com','123456',1.00,'Entrepreneurial Visionary Accelerator, Digital Systems Architecture','2026-08-07 05:05:09','{\"trailblazersCount\":0,\"visioneersCount\":0,\"strategistsCount\":0,\"studentNames\":{}}',NULL,NULL,NULL,NULL,NULL,0),(4,'Riju ',23,'Cabo Verde','sab','ri@gmail.com','123456',1.00,'Diplomatic Policy Leadership, Legal Advocacy Professional','2026-08-07 06:24:10','{}',NULL,NULL,NULL,NULL,NULL,0),(5,'Salman',21,'Brazil','ron','sam@gmail.com','123456',1.00,'Global Oratory and Communication Certificate','2026-08-07 10:12:51',NULL,NULL,NULL,NULL,NULL,NULL,0),(6,'TEST',0,'INDIA','KOTTAYAM','tes@gmail.com','school_default_password',1.00,'School Competition - Single Team','2026-08-07 10:21:26','{\"trailblazersCount\":1,\"visioneersCount\":0,\"strategistsCount\":0,\"studentNames\":{\"trailblazers-0-1\":\"shi\",\"trailblazers-0-2\":\"shy\",\"trailblazers-0-3\":\"shu\",\"trailblazers-0-4\":\"sha\",\"trailblazers-0-5\":\"sho\"}}',NULL,NULL,NULL,'Alma Yousuf','alm@gmail.com',0),(7,'sabu',25,'Fiji','edathvaa','sabu@gmail.com','123456',1.00,'Entrepreneurship and Innovation Accelerator','2026-08-07 10:47:42',NULL,NULL,NULL,NULL,NULL,NULL,0),(8,'minnu',20,'Czechia','edatva','min@gmail.com','123456',1.00,'Digital Media Strategy','2026-08-07 11:38:50',NULL,NULL,NULL,NULL,NULL,NULL,0),(9,'Renjini Sabu',25,'Colombia','kuttanad','ren@gmail.com','123456',1.00,'Entrepreneurship and Innovation Accelerator','2026-08-11 04:28:16',NULL,NULL,NULL,NULL,NULL,NULL,0),(10,'Thengha',0,'India','idukki','te@gmail.com','school_default_password',1.00,'School Competition - Single Team','2026-08-11 05:41:08','{\"trailblazersCount\":1,\"visioneersCount\":0,\"strategistsCount\":0,\"studentNames\":{\"trailblazers-0-1\":\"am\",\"trailblazers-0-2\":\"um\",\"trailblazers-0-3\":\"em\",\"trailblazers-0-4\":\"om\",\"trailblazers-0-5\":\"qm\"}}',NULL,NULL,NULL,'King','ki@gmail.com',0),(11,'varthath',0,'angola','tambak','va@gmail.com','school_default_password',1.00,'School Competition - Single Team','2026-08-11 08:27:21','{\"trailblazersCount\":1,\"visioneersCount\":0,\"strategistsCount\":0,\"studentNames\":{\"trailblazers-0-1\":\"tu\",\"trailblazers-0-2\":\"ti\",\"trailblazers-0-3\":\"ta\",\"trailblazers-0-4\":\"to\",\"trailblazers-0-5\":\"te\"}}',NULL,NULL,NULL,'Salman ','sa@gmail.com',0);
 /*!40000 ALTER TABLE `registrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,4 +260,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-07 12:39:41
+-- Dump completed on 2026-08-11 14:44:17
